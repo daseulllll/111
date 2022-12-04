a6e494f029ec6a66e353d4c4320646d76801f3cf
김다슬 <ektmf4785@naver.com>
2022-12-04 22:51:45 +0900

    학점계산

---
+++ IdeaProjects/oop-practice2/src/main/java/com/example/gradecalculator/Courses.java
@@ -0,0 +1,34 @@
+package com.example.gradecalculator;
+
+import java.util.List;
+
+public class Courses  {
+
+    private final List<Course> courses;
+
+    public Courses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+
+    public double multiplyCreditAndCourseGrade() {
+
+        return courses.stream()
+                .mapToDouble(Course::multiplyCreditAndCourseGrade)
+                .sum();
+
+        /*
+        double multipliedCreditAndCourseGrade = 0;
+        for(Course course: courses){
+//           multipliedCreditAndCourseGrade += course.getCredit() * course.getGradeToNumber();
+            multipliedCreditAndCourseGrade += course.multiplyCreditAndCourseGrade();
+        }
+        return multipliedCreditAndCourseGrade;*/
+    }
+
+    public int calculatedTotalCompletedCredit() {
+        return courses.stream()
+            .mapToInt(Course::getCredit)
+            .sum();
+    }
+}
---
+++ IdeaProjects/oop-practice2/src/main/java/com/example/gradecalculator/GradeCalculator.java
@@ -0,0 +1,38 @@
+package com.example.gradecalculator;
+
+import java.util.List;
+
+public class GradeCalculator {
+
+
+    private final Courses courses;
+
+    public GradeCalculator(List<Course> courses) {
+        this.courses = new Courses(courses);
+    }
+
+
+    public GradeCalculator(Courses courses) {
+        this.courses = courses;
+    }
+/*
+학점계산기 구현
+• 요구사항
+• 평균학점 계산 방법 = (학점수×교과목 평점)의 합계/수강신청 총학점 수
+• MVC패턴(Model-View-Controller) 기반으로 구현한다.
+• 일급 컬렉션 사용
+
+   //이수한 과목을 전달하여 평균학점 계산 요청 --> 학점 계산기 --> (학점수×교과목 평점)의 합계 -->과목(코스)
+   //                                                 --> 수강신청 총학점 수        -->과목(코스)
+ */
+
+    public double calculateGrade() {
+//      (학점수×교과목 평점)의 합계
+       double totalMultipliedCreditAndCourseGrade = courses.multiplyCreditAndCourseGrade();
+
+//      수강신청 총학점 수
+       int totalCompletedCredit = courses.calculatedTotalCompletedCredit();
+
+        return totalMultipliedCreditAndCourseGrade / totalCompletedCredit ;
+    }
+}
